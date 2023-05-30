63e87497f42b12649b941781c87967a3e802235a
Hemin Jeong <jhemin0415@gmail.com>
2023-05-30 13:00:59 +0900

    출력함수 실습 (점심시간 이전)

--- default.js
+++ default.js
@@ -1,9 +1,38 @@
-document.write('Hello world 2');
-document.write("<h1>Welcome to JS Program</h1>");
-document.write("<h2>Welcome to JS Program</h2>");
 
+console.log(123, typeof 123);
+console.log(123.5,typeof 123.5);
+console.log("123", typeof "123");
+console.log(true, typeof true);
+console.log(false, typeof false);
+
+
+var car;
+console.log(car);
+var car = "";
+console.log(car);
+
+var person = {firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue"};
+console.log(person);
+console.log(typeof person);
+person = null;
+console.log(typeof person);
+
+
+
+
+
+
+//document.write('Hello world 2');
+//document.write("<h1>Welcome to JS Program</h1>");
+//document.write("<h2>Welcome to JS Program</h2>");
+
+
+
+/*
 console.warn("welcome to js");
 console.log('hello world');
-
+console.info('Welcome')
 
 alert('welcome to js');
+*/
+
