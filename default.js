830115aceaa22cd5226254ab8047b281e9e72807
Hemin Jeong <jhemin0415@gmail.com>
2023-05-30 14:04:54 +0900

    출력함수 실습 upper, lower, slice

--- default.js
+++ default.js
@@ -1,23 +1,68 @@
 
-console.log(123, typeof 123);
-console.log(123.5,typeof 123.5);
-console.log("123", typeof "123");
-console.log(true, typeof true);
-console.log(false, typeof false);
+
+var text = prompt("Enter your name: ");
+document.write('Your name : ' + text + "<br>");
+
+var len = text.length;
+document.write("Number of characters : " + len + '<br>');
+
+document.write(text.charAt(2) + '<br>');
+
+document.write(text.toUpperCase() + '<br>')
+document.write(text.toLowerCase() + '<br>')
+
+var text1 = " hi";
+var text2 = "bye";
+var text3 = text1.concat(text2);
+
+document.write(text3 + "<br>");
 
 
-var car;
-console.log(car);
-var car = "";
-console.log(car);
+var text4 = "hello";
+var result = text4.slice(0, 2);
+document.write(result + '<br>');
 
-var person = {firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue"};
-console.log(person);
-console.log(typeof person);
-person = null;
-console.log(typeof person);
+//var name = "이승훈";
+//var age = 29;
+//var cgpa = 3.92;
+//var lineBreak = "<br/>";
+//
+//document.write("이름 : " + name + lineBreak);
+//document.write("나이 : " + age + lineBreak);
+//document.write("학점 : " + cgpa + lineBreak);
+//
+//
+//var lastName = '홍';
+//var firstName = '길동';
+//
+//var fullName = lastName + firstName;
+//
+//console.log(fullName);
+//console.log('Today is' + ' a ' + 'beautiful day');
+//console.log('My name is ' + fullName);
 
 
+
+//
+//console.log(123, typeof 123);
+//console.log(123.5,typeof 123.5);
+//console.log("123", typeof "123");
+//console.log(true, typeof true);
+//console.log(false, typeof false);
+//
+//
+//var car;
+//console.log(car);
+//var car = "";
+//console.log(car);
+//
+//var person = {firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue"};
+//console.log(person);
+//console.log(typeof person);
+//person = null;
+//console.log(typeof person);
+//
+//
 
 
 
