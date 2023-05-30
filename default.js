0618a9605ae270dfd044cd0f255b75a687443cf4
Hemin Jeong <jhemin0415@gmail.com>
2023-05-30 11:24:26 +0900

    출력함수 실습

--- default.js
+++ default.js
@@ -1,1 +1,9 @@
 document.write('Hello world 2');
+document.write("<h1>Welcome to JS Program</h1>");
+document.write("<h2>Welcome to JS Program</h2>");
+
+console.warn("welcome to js");
+console.log('hello world');
+
+
+alert('welcome to js');
