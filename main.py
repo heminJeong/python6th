9b4f40c3faf0fa31508bf227a0ee4bad2a8e52a6
Hemin Jeong <jhemin0415@gmail.com>
2023-06-05 13:11:35 +0900

    파이썬 실습 사칙연산

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# 변수 선언
-a = 10
-b = 3.14
-c = 'hello world!'
-d = [1,2,3]
-e = (4, 5, 6)
-f = {7, 8 ,9}
-g = {'apple': 1, "banana" : 2, "orange" : 3}
+a = 4
+b = 2
+total = a + b
+print("total = a + b: ", total)
 
-print(type(a))
-print(type(b))
-print(type(c))
-print(type(d))
-print(type(e))
-print(type(f))
-print(type(g))
+
+total = a - b
+print("total = a - b: ", total)
+
+
+total = a * b
+print("a * b : ", total)
+
+total = a / b
+print("a / b : ", total)
+
+
+print("a ** b : ", a ** b )
+
+print("a // b : ", a // b)
+
