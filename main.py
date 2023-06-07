6baf334e06c3c835a8edbd95b4cfffd088353ee4
Hemin Jeong <jhemin0415@gmail.com>
2023-06-07 15:36:17 +0900

    함수 파라미터

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
-print("인자가 없는 함수")
+def disp(sh):
+    print(type(sh))
+    print("Disp Function" + sh())
 
-def disp():
-    name = "멋쟁이사자"
-    print("Welcome to", name)
+def show():
+    return " Show Function"
 
-
-print("함수 실행")
-disp()
-disp()
-disp()
-
-def add(y):
-    x = 10.2334
-    print(x + y)
-    print(f"Formatted Output {x + y:5.2f}")
-
-add(20)
+disp(show)
