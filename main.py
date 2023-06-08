f4cd3687f1a533fe6acc1e3d3381d1c01bc5a1a4
Hemin Jeong <jhemin0415@gmail.com>
2023-06-08 15:39:16 +0900

    재귀함수 호출

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-def val(x):
-    print("Inside : ", x, id(x))
-    x += 1
-    print("Inside After : ", x, id(x))
+import sys
+sys.setrecursionlimit(3000)
 
-x = 10
-print("Before Calling : ", x, id(x))
-val(x)
-print("After Calling : ", x, id(x))
+i = 0
+def myfunc():
+    global i
+    i += 1
+    print("My Function : ", i)
+    myfunc()
+
+myfunc()
