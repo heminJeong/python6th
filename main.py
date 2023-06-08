fac93a25fc1055c32d798aeddf108929e034a54c
Hemin Jeong <jhemin0415@gmail.com>
2023-06-08 10:27:00 +0900

    함수 가변인자

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-def disp(sh):
-    print("Disp Function")
-    return sh
+def pw(x, y):
+    z = x ** y
+    print(z)
 
-def show():
-    return "Show Function"
+pw(2, 5)
+pw(5, 2)
 
-r_sh = disp(show)
-print(r_sh())
+def show(name: str, age: int):
+    print(f"Name: {name} Age: {age}")
+
+show(name='정해민', age=30)
+
+def add(x, y):
+    z = x + y
+    print("Addition : ", z)
+
+add(5, 2)
+
+def add(*num):
+    z = num[0] + num[1] + num[2]
+    print("Addition *: ", z)
+
+add(5, 2, 4)
+
+def add(x, *num):
+    z = x + num[0] + num[1]
+    print("Addition x *: ", z)
+
+add(5, 2, 4)
