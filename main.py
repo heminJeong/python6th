09deb9ee2007a6c90d0e0a5dc2bffc734b315bd8
Hemin Jeong <jhemin0415@gmail.com>
2023-06-08 15:25:07 +0900

    Call by Reference 참조를 이용한 함수 실행

--- main.py
+++ main.py
@@ -1,35 +1,9 @@
-stu = {101: 'Kim', 102: 'Bae', 103: "Hong"}
-fees = {'Kim': 2000, 'Bae': 3000, 'Hong': 8000}
-print(stu[101])
-print(stu[102])
-print(stu[103])
+def val(lst):
+    print("Inside Function Before Append : ", lst, id(lst))
+    lst.append(4)
+    print("Inside Function After Append : ", lst, id(lst))
 
-print(fees['Kim'])
-print(fees['Bae'])
-print(fees['Hong'])
-
-stu[101] = 'Python'
-print(stu)
-
-stu[104] = '멋쟁이사자'
-
-print(stu)
-
-del stu[102]
-
-print(stu)
-
-new_stu = stu.copy()
-
-key = (101, 102, 103)
-value = '멋쟁이사자'
-
-
-new_stu = dict.fromkeys(key, value)
-print(new_stu)
-
-print(stu[101])
-print(stu.get(101))
-print(stu.items())
-print(stu.keys())
-print(stu.values())
+lst = [1, 2, 3]
+print("Before Calling Function : ", lst, id(lst))
+val(lst)
+print("After Calling Function : ", lst, id(lst))
