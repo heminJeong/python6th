edd66399865afa1dfc0df92fba06ea67cc54d972
Hemin Jeong <jhemin0415@gmail.com>
2023-06-08 15:28:58 +0900

    Call by Value 참조를 이용한 함수 호출 실습

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def val(lst):
-    print("Inside Function Before Append : ", lst, id(lst))
-    lst.append(4)
-    print("Inside Function After Append : ", lst, id(lst))
+def val(x):
+    print("Inside : ", x, id(x))
+    x += 1
+    print("Inside After : ", x, id(x))
 
-lst = [1, 2, 3]
-print("Before Calling Function : ", lst, id(lst))
-val(lst)
-print("After Calling Function : ", lst, id(lst))
+x = 10
+print("Before Calling : ", x, id(x))
+val(x)
+print("After Calling : ", x, id(x))
