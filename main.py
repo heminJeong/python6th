5aa6a2eb3db726dde67318466e259605fd00b3d7
Hemin Jeong <jhemin0415@gmail.com>
2023-06-08 10:48:10 +0900

    Global 키워드

--- main.py
+++ main.py
@@ -27,3 +27,41 @@ def add(x, *num):
     print("Addition x *: ", z)
 
 add(5, 2, 4)
+
+def add(**num):
+    z = num['a'] + num['b'] + num['c']
+    print('Addition : ', z)
+
+add(a = 5, b = 2, c = 4, d= 5)
+
+
+a = 50
+
+def show():
+    x = 10
+    print(x)
+    print(a)
+
+show()
+
+print("Global Variable a : ", a)
+
+def show2():
+    global a
+    print("show2-A :", a)
+    a = 20
+    print("show2-A2 :", a)
+
+show2()
+print("A : ", a)
+
+
+i = 0
+def myfun():
+    print("My Function i : ", i)
+
+
+    print("My function", a)
+
+myfun()
+print("Global Variable a : ", a)
