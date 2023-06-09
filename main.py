937c70a6bbf64f1a2efd53f6a404f22fea34ce50
Hemin Jeong <jhemin0415@gmail.com>
2023-06-09 14:02:36 +0900

    여러줄 읽기

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-with open('example.txt', 'w') as file_object:
-    content = """This tis a multiline string.
-Python is a versatile language.
-It is easy to learn and use."""
-    print(content)
-    file_object.write(content)
+with open('example.txt', 'r') as file_object:
+    lines = file_object.readlines()
+    for line in lines:
+        print(line.strip())
