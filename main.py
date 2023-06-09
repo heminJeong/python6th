1f3e2d335d057129ab3ba69166508dab98f3c1c1
Hemin Jeong <jhemin0415@gmail.com>
2023-06-09 13:48:54 +0900

    파일 쓰기 (멀티라인)

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-print('파일 열기')
-file_object = open('example.txt', 'r')
-
-
-print("현재 파일 위치 확인")
-position = file_object.tell()
-print("Current Position : ", position)
-
-print("파일 포인터 위치 변경")
-file_object.seek(7)
-
-print("변경된 위치 확인")
-position = file_object.tell()
-print("New position : ", position)
-
-print("파일 닫기")
-file_object.close()
+with open('example.txt', 'w') as file_object:
+    content = """This tis a multiline string.
+Python is a versatile language.
+It is easy to learn and use."""
+    print(content)
+    file_object.write(content)
