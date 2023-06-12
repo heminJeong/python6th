9ca17d1d01b78e503039bafe10dba17b9fab16ae
Hemin Jeong <jhemin0415@gmail.com>
2023-06-12 13:20:25 +0900

    Custom Exception 생성, 발생

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+class CusteomException(Exception):
+    def __init__(self, message):
+        self.message = message
+
 try:
-    number = 5 + "not a number"
-except ValueError:
-    print("Error : Invalid value.")
-except TypeError:
-    print("Error : Invalid type.")
+    raise CusteomException("This is a custom exception.")
+except CusteomException as e:
+    print(f"Error : {e.message}")
