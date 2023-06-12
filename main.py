410d93cd71856a829e82f2d1358390bd421e6829
Hemin Jeong <jhemin0415@gmail.com>
2023-06-12 15:04:16 +0900

    enumerate 실습

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-a = ['a1', 'a2', 'a3']
+from pprint import pprint
 
-for i, v in enumerate(a):
-    print([i, v])
-print('-------------------')
-i = 0
-for v in a:
-    print([i, v])
-    i += 1
-print('-------------------')
-for i in range(len(a)):
-    print([i, a[i]])
+pprint(locals())
