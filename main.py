a78dd9d9aad6feda6fbb09bdfdce47faf0947405
Hemin Jeong <jhemin0415@gmail.com>
2023-06-16 14:58:26 +0900

    세렝게티 동물 테스트

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-class MyQueue:
+from bs4 import BeautifulSoup
+import requests
 
-    def __init__(self):
-        self.input = []
-        self.output = []
 
-    def push(self, x: int) -> None:
-        self.input.append(x)
-
-    def pop(self) -> int:
-        self.peek()
-        return self.output.pop()
-
-    def peek(self) -> int:
-        if not self.output:
-            while self.input:
-                self.output.append(self.input.pop())
-        return self.output[-1]
-
-    def empty(self) -> bool:
-        return self.input == [] and self.output == []
+with open("daum.html", "r") as f:
+    data = f.read()
+    print(BeautifulSoup(f, 'html.parser'))
