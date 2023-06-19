dc38c87b251db6467c9c4529cb2d06db2564fd93
Hemin Jeong <jhemin0415@gmail.com>
2023-06-19 17:25:37 +0900

    html get, image paser

--- main.py
+++ main.py
@@ -1,38 +1,37 @@
-from collections import deque
-
-graph = {
-    'A': ['B', 'C'],
-    'B': ['A', 'D', 'E'],
-    'C': ['A', 'F'],
-    'D': ['B'],
-    'E': ['B', 'F'],
-    'F': ['C', 'E']
-}
-
-visited = set()
-
-def bfs_iterative(start_node):
-    queue = deque([start_node])
-    while queue:
-        node = queue.popleft()
-        if node not in visited:
-            print(node, end=' ')
-            visited.add(node)
-            queue.extended(graph[node])
-
-def dfs_iterative(start_node: str) -> None:
-    stack = [start_node]
-
-    while stack:
-        node = stack.pop()
-        if node not in visited:
-            print(node, end=' ')
-            visited.add(node)
-            stack.extend(reversed(graph[node]))
+from urllib.request import urlopen
+from html.parser import HTMLParser
 
 
-snode = 'A'
-dfs_iterative(snode)
-visited.clear()
-print('\n------------')
-dfs_iterative(snode)
+
+class ImageParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.result = []
+    def handle_starttag(self, tag, attrs):
+        if tag != 'img':
+            return
+        if not hasattr(self, 'result'):
+            self.result = []
+        for name, value in attrs:
+            if name == 'src':
+                self.result.append(value)
+
+def parse_image(data):
+    parser = ImageParser()
+    parser.feed(data)
+    data_set = set(x for x in parser.result)
+    return data_set
+
+def main():
+    url = 'https://google.co.kr'
+    with urlopen(url) as f:
+        charset = f.headers.get_params('charset')[1][1]
+        print(charset)
+        data = f.read().decode(charset)
+
+    data_set = parse_image(data)
+    print("\n>>> Fetch Images from", url)
+    print("\n".join(sorted(data_set)))
+
+if __name__ == "__main__":
+    main()
