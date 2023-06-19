3f9b80551c06b1665ad63677a69217657809f7ad
Hemin Jeong <jhemin0415@gmail.com>
2023-06-19 13:44:07 +0900

    그래프 알고리즘

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
-from bs4 import BeautifulSoup
-import requests
+from collections import deque
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+visited = set()
+
+def bfs_iterative(start_node):
+    queue = deque([start_node])
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            print(node, end=' ')
+            visited.add(node)
+            queue.extended(graph[node])
+
+def dfs_iterative(start_node: str) -> None:
+    stack = [start_node]
+
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            print(node, end=' ')
+            visited.add(node)
+            stack.extend(reversed(graph[node]))
 
 
-with open("daum.html", "r") as f:
-    data = f.read()
-    print(BeautifulSoup(f, 'html.parser'))
+snode = 'A'
+dfs_iterative(snode)
+visited.clear()
+print('\n------------')
+dfs_iterative(snode)
